4b55d64fbdbcdd203426d22d380d8a92119d5903
Sagar Gautam <unstopable.sagar@gmail.com>
2024-04-27 15:59:36 +0545

    Hide the ecommerce django project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,14 +17,14 @@ const Projects = () => {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        <a href="#" target="_blank" rel="noopener noreferrer">
+        {/* <a href="#" target="_blank" rel="noopener noreferrer">
           <ProjectsCard
             title="E-commerce Website - Django, Bootstrap"
             // des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             //     Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
             src={projectOne}
           />
-        </a>
+        </a> */}
         <a href="https://unstopable-sagar.github.io/react-ecommerce-app/" target="_blank" rel="noopener noreferrer">
           <ProjectsCard
             title="E-commerce Website - React, Bootstrap"
